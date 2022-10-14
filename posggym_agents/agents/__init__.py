4f0dbf3d0bd87baa6e4e604b8799c4b9f12e24eb
Jonathon <Jonathon.schwartz12@gmail.com>
2022-10-14 07:50:43 -0400

    added PP10-P4-p3-s3-coop sp agents

--- posggym_agents/agents/__init__.py
+++ posggym_agents/agents/__init__.py
@@ -14,6 +14,7 @@ from posggym_agents.agents import pursuitevasion16x16_v0
 from posggym_agents.agents import predatorprey10x10_P2_p3_s2_coop_v0
 from posggym_agents.agents import predatorprey10x10_P3_p3_s2_coop_v0
 from posggym_agents.agents import predatorprey10x10_P4_p3_s2_coop_v0
+from posggym_agents.agents import predatorprey10x10_P4_p3_s3_coop_v0
 
 
 # Generic Random Policies
@@ -55,6 +56,7 @@ for policy_spec in pursuitevasion8x8_v0.POLICY_SPECS.values():
 for policy_spec in pursuitevasion16x16_v0.POLICY_SPECS.values():
     register_spec(policy_spec)
 
+
 # PredatorPrey
 # ------------
 for policy_spec in predatorprey10x10_P2_p3_s2_coop_v0.POLICY_SPECS.values():
@@ -64,4 +66,7 @@ for policy_spec in predatorprey10x10_P3_p3_s2_coop_v0.POLICY_SPECS.values():
     register_spec(policy_spec)
 
 for policy_spec in predatorprey10x10_P4_p3_s2_coop_v0.POLICY_SPECS.values():
+    register_spec(policy_spec)
+
+for policy_spec in predatorprey10x10_P4_p3_s3_coop_v0.POLICY_SPECS.values():
     register_spec(policy_spec)
